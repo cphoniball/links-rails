67bf907198c61bb73e14a117838e7c46170f346f
Chris Honiball <chris@chrishoniball.com>
2019-10-22 18:25:14 -0500

    Signup page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :email, presence: true, uniqueness: true
-  validates :username, presence: true
   validates :password, confirmation: true, length: { minimum: 8 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "pages#home"
-  resources :users
+  # TODO: This overlaps with the devise registration routes, move this into a /admin nested resource
   resources :link_pages, shallow: true do
     resources :links
   end
+
+  scope "/admin" do
+    resources :users
+  end
+
+  get "*path", to: "pages#not_found"
 end
