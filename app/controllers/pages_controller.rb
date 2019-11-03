306c4168c7ab1880cbfec94402e80c3af4526251
Chris Honiball <chris@chrishoniball.com>
2019-11-03 15:04:03 -0600

    Clean up styles for sign in/sign out

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,12 @@
 class PagesController < ApplicationController
   def home
+    redirect_to profile_path if current_user
   end
 
   def not_found
+  end
+
+  def profile
   end
 
   def link_page
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "pages#home"
+  get "/profile", to: "pages#profile", as: :profile
   get "/link_page/:slug", to: "pages#link_page", as: :visit_link_page
   get "/link/:id/visit", to: "links#visit", as: :visit_link
 
